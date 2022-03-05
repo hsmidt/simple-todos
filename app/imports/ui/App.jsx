3c60d8ae0f6e1fcee8a1e538fba32ff146528256
Holm Smidt <hsmidt@hawaii.edu>
2022-03-05 00:05:41 -1000

    Section 4

--- app/imports/ui/App.jsx
+++ app/imports/ui/App.jsx
@@ -4,6 +4,16 @@ import { TasksCollection } from '../api/TasksCollection';
 import { Task } from './Task';
 import { TaskForm } from './TaskForm';
 
+const toggleChecked = ({ _id, isChecked }) => {
+  TasksCollection.update(_id, {
+    $set: {
+      isChecked: !isChecked,
+    }
+  })
+}
+
+const deleteTask = ({ _id }) => TasksCollection.remove(_id);
+
 export const App = () => {
   const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
 
@@ -12,7 +22,11 @@ export const App = () => {
       <h1>Welcome to Meteor!</h1>
       <TaskForm/>
       <ul>
-        {tasks.map(task => <Task key={task._id} task={task}/>)}
+        { tasks.map(task => <Task
+          key={task._id} task={task}
+          onCheckboxClick={toggleChecked}
+          onDeleteClick={deleteTask}
+        />)}
       </ul>
     </div>
   );
--- app/imports/ui/Task.jsx
+++ app/imports/ui/Task.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-export const Task = ({ task }) => {
+export const Task = ({ task, onCheckboxClick, onDeleteClick }) => {
   return (
-    <li>{ task.text }</li>
+    <li>
+      <input
+        type='checkbox'
+        checked={!!task.isChecked}
+        onClick={() => onCheckboxClick(task)}
+        readOnly
+      />
+      <span>{ task.text }</span>
+      <button onClick={ () => onDeleteClick(task) }>&times;</button>
+    </li>
   )
 };
